cf483bd472218cba5cf7b520356e55d1b9bc5892
Jorge Flores <floreshjf@gmail.com>
2019-04-12 02:36:01 -0400

    los test case para la creacion, edicion y eliminacion

--- src/main/java/org/umssdiplo/automationv01/core/managepage/catalogo/clickCatalogo.java
+++ src/main/java/org/umssdiplo/automationv01/core/managepage/catalogo/clickCatalogo.java
@@ -4,10 +4,62 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.umssdiplo.automationv01.core.managepage.BasePage;
 import org.umssdiplo.automationv01.core.utils.CommonEvents;
+import org.umssdiplo.automationv01.core.utils.PropertyAccessor;
+
+import java.util.List;
 
 public class clickCatalogo extends BasePage {
-    @FindBy(css = ".fa.fa-list.fa-lg")
+    //    @FindBy(css = ".fa.fa-list.fa-lg")
+    @FindBy(css = ".dropdown-toggle")
     private WebElement catalogoButonHeader;
+
+    @FindBy(xpath = "//*[@id=\"ssiga-navbar-collapse\"]/ul/ul/li[3]/ul/li[2]/a")
+    private WebElement employeeMenuSelect;
+
+    @FindBy(css = ".btn.btn-success")
+    private WebElement addEmployee;
+
+    //    @FindBy(css = ".form-control.ng-untouched.ng-pristine.ng-valid")
+    @FindBy(name = "firstName")
+    private WebElement addEmployeeFirstName;
+
+    @FindBy(name = "lastName")
+    private WebElement addEmployeeLastName;
+
+    @FindBy(name = "ci")
+    private WebElement addEmployeeCI;
+
+    @FindBy(name = "address")
+    private WebElement addEmployeeAddress;
+
+    @FindBy(name = "phone")
+    private WebElement addEmployeephone;
+
+    @FindBy(name = "email")
+    private WebElement addEmployeeEmail;
+
+    @FindBy(name = "salary")
+    private WebElement addEmployeeSalary;
+
+    @FindBy(name = "gender")
+    private WebElement addEmployeeGender;
+
+    @FindBy(name = "birthDate")
+    private WebElement addEmployeeBirthDate;
+
+    @FindBy(name = "employeeTypeId")
+    private WebElement addEmployeeTypeId;
+
+    //    @FindBy(css = ".btn.btn-primary")
+    // @FindBy(xpath = "//html/body/modal-container/div/div/app-add-employee/div/div[2]/form/div[8]/div/button[1]")
+    @FindBy(css = "button[type='submit'")
+    private WebElement addEmployeeBtn;
+
+    @FindBy(xpath = "//table/tbody/tr[7]/td[9]/button[2]")
+    private WebElement editEmployeeBtn;
+
+    @FindBy(xpath = "//table/tbody/tr[7]/td[9]/button[1]")
+    private WebElement delEmployee;
 
 //    @FindBy(name = "password")
 //    private WebElement passwordInputField;
@@ -21,5 +73,69 @@ public class clickCatalogo extends BasePage {
 //        CommonEvents.setInputField(usernameInputField, username);
 //        CommonEvents.setInputField(passwordInputField, password);
         CommonEvents.clickButton(catalogoButonHeader);
+    }
+
+    public void clickMenuEmp() {
+        CommonEvents.clickButton(employeeMenuSelect);
+    }
+
+    public void clickAddEmpl() {
+        CommonEvents.clickButton(addEmployee);
+    }
+
+    public void setEmployee() {
+        String username = PropertyAccessor.getInstance().getUser();
+        CommonEvents.setInputField(addEmployeeFirstName, username);
+    }
+
+    public void setEmployee2(List<List<String>> list) {
+        String username = list.get(0).get(0);
+        String lastname = list.get(0).get(1);
+        String ci = list.get(0).get(2);
+        String address = list.get(0).get(3);
+        String phone = list.get(0).get(4);
+        String email = list.get(0).get(5);
+        String salary = list.get(0).get(6);
+        String gender = list.get(0).get(7);
+        String date = list.get(0).get(8);
+        String cargo = list.get(0).get(9);
+
+
+        addEmployeeFirstName.sendKeys(username);
+        addEmployeeLastName.sendKeys(lastname);
+        addEmployeeCI.sendKeys(ci);
+        addEmployeeAddress.sendKeys(address);
+        addEmployeephone.sendKeys(phone);
+        addEmployeeEmail.sendKeys(email);
+        addEmployeeGender.sendKeys(gender);
+        addEmployeeBirthDate.sendKeys(date);
+        addEmployeeTypeId.sendKeys(cargo);
+        addEmployeeSalary.sendKeys(salary);
+        addEmployeeSalary.submit();
+        //CommonEvents.setInputField(addEmployeeFirstName, username);
+
+//        CommonEvents.clickButton(addEmployeeBtn);
+    }
+
+    public void clickDeleteEmpl() {
+        CommonEvents.clickButton(delEmployee);
+
+
+    }
+
+    public void clickAddEmplBtn() {
+
+//        CommonEvents.clickButton(addEmployeeBtn);
+        // addEmployeeBtn.submit();
+        CommonEvents.pressEnterKey(addEmployeeBtn);
+    }
+
+    public void editEmployee(List<List<String>> list) {
+        String newLastName = list.get(0).get(0);
+
+        CommonEvents.clickButton(editEmployeeBtn);
+        addEmployeeLastName.clear();
+        addEmployeeLastName.sendKeys(newLastName);
+
     }
 }
--- src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionPHPtravel.java
+++ src/test/java/org/umssdiplo/automationv01/stepdefinitionproject/StepsDefinitionPHPtravel.java
@@ -1,14 +1,19 @@
 package org.umssdiplo.automationv01.stepdefinitionproject;
 
+import cucumber.api.DataTable;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import org.openqa.selenium.WebDriver;
 import org.umssdiplo.automationv01.core.managepage.Login.Login;
 import org.umssdiplo.automationv01.core.managepage.catalogo.clickCatalogo;
 import org.umssdiplo.automationv01.core.utils.LoadPage;
 
+import java.util.List;
+
 public class StepsDefinitionPHPtravel {
     private Login login;
     private clickCatalogo clickCatalogo;
+    WebDriver driver;
 
     @Given("^'PHP travel' page is loaded$")
     public void phpTravelPageIsLoaded() throws Throwable {
@@ -25,8 +30,47 @@ public class StepsDefinitionPHPtravel {
         clickCatalogo = LoadPage.catalogoPage();
     }
 
-    @And("^click 'Catalogo' page in 'buton'$")
+    @And("^click 'Employee' page in 'buton'$")
     public void clickCatalogoPageInButon() {
         clickCatalogo.clickButtonH();
+    }
+
+    @And("^click on 'Employee list' menu in 'header button'$")
+    public void clickOnEmployeeListMenuInHeaderButton() {
+        clickCatalogo.clickMenuEmp();
+    }
+
+    @And("^click on 'add Employee' button in 'employee list' page$")
+    public void clickOnAddEmployeeButtonInEmployeeListPage() {
+        clickCatalogo.clickAddEmpl();
+    }
+
+    @And("^set first name in 'add employee' page$")
+    public void setFirstNameInAddEmployeePage() {
+        clickCatalogo.setEmployee();
+    }
+
+    @And("^click 'delete employee' in 'employee list' page$")
+    public void clickDeleteEmployeeInEmployeeListPage() {
+        clickCatalogo.clickDeleteEmpl();
+    }
+
+    @And("^set personal dates in 'add employee' pageLoad$")
+    public void setFirstNameInAddEmployeePage(DataTable dt) {
+        List<List<String>> list = dt.raw();
+        // driver.findElement(By.name("firstName")).sendKeys(list.get(0).get(0));
+        clickCatalogo.setEmployee2(list);
+    }
+
+    @And("^click ' submit button' in 'add employee' page$")
+    public void clickSubmitButtonInAddEmployeePage() throws Throwable {
+        clickCatalogo.clickAddEmplBtn();
+    }
+
+    @And("^click ' edit button' in 'employee list' page for edit 'lastmane' value$")
+    public void clickEditButtonInEmployeeListPageForEditLastmaneValue(DataTable dt) {
+        List<List<String>> list = dt.raw();
+        clickCatalogo.editEmployee(list);
+        clickCatalogo.clickAddEmplBtn();
     }
 }
